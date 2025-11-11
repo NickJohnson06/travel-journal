63f8a2c0d3aca400aaa203db6d4e1dcb138c1c7a
Nick <acejohnny06@gmail.com>
2025-11-11 14:38:36 -0500

    debug server when tested

--- server/src/models/Trip.js
+++ server/src/models/Trip.js
@@ -11,6 +11,4 @@ const TripSchema = new mongoose.Schema({
   imageUrl: { type: String, default: "" }
 }, { timestamps: true });
 
-TripSchema.index({ userId: 1 });
-
 export default mongoose.model('Trip', TripSchema);
